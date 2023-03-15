46afa53931260b32d93abb311937e151cddaaca9
JosephSBoyle <48555120+JosephSBoyle@users.noreply.github.com>
2023-03-15 12:37:15 +0000

    Add a test that attention is the same for two equal vectors

--- self_attention/self_attention.py
+++ self_attention/self_attention.py
@@ -18,7 +18,8 @@ def apply(x: NDArray) -> NDArray:
     """Apply the self-attention mechanism to the input sequence to
     generate an output sequence.
     """
-    w = np.dot(x.T, x) / np.sqrt(x.shape[0])
+    w = np.dot(x.T, x)
+    w /= np.sqrt(x.shape[0])
 
     # w_prime should be symmetric
     # two tokens should pay equal attention to each other.
@@ -32,3 +33,14 @@ def apply(x: NDArray) -> NDArray:
         w[i] = _softmax(row)
 
     return np.dot(x, w)
+
+
+if __name__ == "__main__":
+    x1 = np.array([1., 0., 0.])
+    x2 = np.array([1., 0., 0.])
+    x3 = np.array([0., 1., 1.])
+    x = np.stack((x1, x2, x3), axis=1)
+    
+    y = apply(x)
+    # The attention of x1 and x2 should be equal, they are the same vectors!
+    assert y[0].all() == y[1].all()
